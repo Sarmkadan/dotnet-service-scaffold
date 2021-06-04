ce0ba55173e4068ba7a77580fc9f6876b543c825
Vladyslav Zaiets <rutova2@gmail.com>
2021-06-04 21:15:52 +0300

    Cleanup in ServiecDiscoveryRecord

---
+++ src/Domain/Models/ServiceDiscoveryRecord.cs
@@ -0,0 +1,153 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DotnetServiceScaffold.Domain.Models;
+
+/// <summary>
+/// Represents a resolved service instance obtained from a discovery backend.
+/// Combines addressing information (host, port, scheme) with health telemetry
+/// and registry metadata so that consumers can select and route to live endpoints.
+/// </summary>
+public sealed class ServiceDiscoveryRecord
+{
+    /// <summary>Gets or sets the unique identifier for this service instance.</summary>
+    [Key]
+    public Guid InstanceId { get; set; } = Guid.NewGuid();
+
+    /// <summary>Gets or sets the logical service name used for discovery lookups.</summary>
+    [Required]
+    [StringLength(200)]
+    public required string ServiceName { get; set; }
+
+    /// <summary>Gets or sets the semantic version advertised by this instance.</summary>
+    [StringLength(50)]
+    public string? Version { get; set; }
+
+    /// <summary>Gets or sets the host name or IP address of this instance.</summary>
+    [Required]
+    [StringLength(253)]
+    public required string Host { get; set; }
+
+    /// <summary>Gets or sets the TCP port this instance is listening on.</summary>
+    [Range(1, 65535)]
+    public int Port { get; set; }
+
+    /// <summary>Gets or sets the URI scheme (http, https, grpc, tcp).</summary>
+    [StringLength(10)]
+    public string Scheme { get; set; } = "https";
+
+    /// <summary>
+    /// Gets or sets the relative weight used for weighted load balancing.
+    /// Higher values attract proportionally more traffic. Valid range: 1–100.
+    /// </summary>
+    [Range(1, 100)]
+    public int Weight { get; set; } = 10;
+
+    /// <summary>
+    /// Gets or sets the failover priority. Lower values are preferred when the
+    /// load-balancing strategy is <see cref="LoadBalancingStrategy.Priority"/>.
+    /// </summary>
+    [Range(0, 65535)]
+    public int Priority { get; set; }
+
+    /// <summary>Gets or sets the current health evaluation for this instance.</summary>
+    public DiscoveryHealthStatus HealthStatus { get; set; } = DiscoveryHealthStatus.Unknown;
+
+    /// <summary>Gets or sets the resolution backend that populated this record.</summary>
+    public DiscoverySource Source { get; set; } = DiscoverySource.Unknown;
+
+    /// <summary>Gets or sets the tags associated with this instance (e.g. version, region, capability).</summary>
+    public List<string> Tags { get; set; } = [];
+
+    /// <summary>Gets or sets arbitrary key-value metadata from the registry entry.</summary>
+    public Dictionary<string, string> Metadata { get; set; } = [];
+
+    /// <summary>Gets or sets when this record was first observed.</summary>
+    public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+    /// <summary>Gets or sets the UTC timestamp of the last successful health confirmation.</summary>
+    public DateTime LastSeenAt { get; set; } = DateTime.UtcNow;
+
+    /// <summary>
+    /// Gets or sets the DNS time-to-live in seconds if this record originated from a DNS lookup.
+    /// <see langword="null"/> for registry-sourced records.
+    /// </summary>
+    public int? DnsTtlSeconds { get; set; }
+
+    /// <summary>Gets or sets the number of consecutive health-check failures for this instance.</summary>
+    public int ConsecutiveFailures { get; set; }
+
+    /// <summary>
+    /// Builds the base endpoint URI for this instance (e.g. <c>https://api.internal:8443</c>).
+    /// </summary>
+    public string ToEndpointUri() => $"{Scheme}://{Host}:{Port}";
+
+    /// <summary>
+    /// Returns <see langword="true"/> when this instance is healthy and its record is not stale.
+    /// </summary>
+    /// <param name="staleThreshold">
+    /// Maximum time since <see cref="LastSeenAt"/> before a record is considered stale.
+    /// Defaults to 5 minutes when omitted.
+    /// </param>
+    public bool IsAlive(TimeSpan? staleThreshold = null)
+    {
+        var threshold = staleThreshold ?? TimeSpan.FromMinutes(5);
+        return HealthStatus is not DiscoveryHealthStatus.Critical
+            && (DateTime.UtcNow - LastSeenAt) < threshold;
+    }
+
+    /// <summary>
+    /// Records a successful health probe, resetting the consecutive failure counter
+    /// and promoting status to <see cref="DiscoveryHealthStatus.Passing"/>.
+    /// </summary>
+    public void RecordHealthy()
+    {
+        HealthStatus = DiscoveryHealthStatus.Passing;
+        ConsecutiveFailures = 0;
+        LastSeenAt = DateTime.UtcNow;
+    }
+
+    /// <summary>
+    /// Records a failed health probe. Status escalates to
+    /// <see cref="DiscoveryHealthStatus.Critical"/> once <paramref name="criticalThreshold"/>
+    /// consecutive failures are reached.
+    /// </summary>
+    /// <param name="criticalThreshold">Failure count that triggers the critical escalation.</param>
+    public void RecordUnhealthy(int criticalThreshold = 3)
+    {
+        ConsecutiveFailures++;
+        HealthStatus = ConsecutiveFailures >= criticalThreshold
+            ? DiscoveryHealthStatus.Critical
+            : DiscoveryHealthStatus.Warning;
+    }
+}
+
+/// <summary>Health status levels for a discovered service instance.</summary>
+public enum DiscoveryHealthStatus
+{
+    /// <summary>Health has not yet been evaluated.</summary>
+    Unknown = 0,
+    /// <summary>The instance is healthy and accepting traffic.</summary>
+    Passing = 1,
+    /// <summary>The instance is degraded but may still serve some requests.</summary>
+    Warning = 2,
+    /// <summary>The instance is unhealthy and must not receive traffic.</summary>
+    Critical = 3
+}
+
+/// <summary>Identifies which resolution backend populated a <see cref="ServiceDiscoveryRecord"/>.</summary>
+public enum DiscoverySource
+{
+    /// <summary>Origin is unknown or unspecified.</summary>
+    Unknown = 0,
+    /// <summary>Resolved from DNS SRV or A/AAAA records.</summary>
+    Dns = 1,
+    /// <summary>Retrieved from an HTTP service registry (Consul-compatible API).</summary>
+    Registry = 2,
+    /// <summary>Registered directly within the current process.</summary>
+    LocalRegistry = 3
+}
---
+++ src/Infrastructure/ServiceDiscovery/ServiceDiscoveryOptions.cs
@@ -0,0 +1,157 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace DotnetServiceScaffold.Infrastructure.ServiceDiscovery;
+
+/// <summary>
+/// Root configuration for the service discovery subsystem.
+/// Bind from the <c>"ServiceDiscovery"</c> section in <c>appsettings.json</c>.
+/// </summary>
+public sealed class ServiceDiscoveryOptions
+{
+    /// <summary>The <c>appsettings.json</c> section key used for configuration binding.</summary>
+    public const string SectionName = "ServiceDiscovery";
+
+    /// <summary>Gets or sets whether the service discovery subsystem is active.</summary>
+    public bool Enabled { get; set; } = true;
+
+    /// <summary>Gets or sets the resolution strategy the discovery engine uses.</summary>
+    public DiscoveryMode Mode { get; set; } = DiscoveryMode.Dns;
+
+    /// <summary>Gets or sets the load-balancing algorithm applied when selecting from healthy instances.</summary>
+    public LoadBalancingStrategy LoadBalancing { get; set; } = LoadBalancingStrategy.RoundRobin;
+
+    /// <summary>Gets or sets how long resolved records are cached before the backend is re-queried.</summary>
+    public TimeSpan CacheTtl { get; set; } = TimeSpan.FromSeconds(30);
+
+    /// <summary>Gets or sets the background poll interval used when watching for instance changes.</summary>
+    public TimeSpan RefreshInterval { get; set; } = TimeSpan.FromSeconds(15);
+
+    /// <summary>Gets or sets the per-call timeout applied to individual resolution requests.</summary>
+    public TimeSpan ResolutionTimeout { get; set; } = TimeSpan.FromSeconds(5);
+
+    /// <summary>Gets or sets DNS-specific resolution settings.</summary>
+    public DnsDiscoveryOptions Dns { get; set; } = new();
+
+    /// <summary>Gets or sets HTTP registry discovery settings.</summary>
+    public RegistryDiscoveryOptions Registry { get; set; } = new();
+
+    /// <summary>Gets or sets self-registration settings for the current service instance.</summary>
+    public SelfRegistrationOptions SelfRegistration { get; set; } = new();
+}
+
+/// <summary>DNS-specific settings for service-instance resolution.</summary>
+public sealed class DnsDiscoveryOptions
+{
+    /// <summary>
+    /// Gets or sets the DNS search domain appended to bare service names.
+    /// For Kubernetes clusters this is typically <c>svc.cluster.local</c>.
+    /// </summary>
+    public string SearchDomain { get; set; } = "svc.cluster.local";
+
+    /// <summary>Gets or sets whether SRV records are queried before falling back to A/AAAA.</summary>
+    public bool PreferSrvRecords { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the IP address of the DNS server used for SRV queries.
+    /// Defaults to <c>127.0.0.53</c> (systemd-resolved stub listener).
+    /// </summary>
+    public string DnsServerAddress { get; set; } = "127.0.0.53";
+
+    /// <summary>Gets or sets the DNS server port (standard is 53).</summary>
+    public int DnsServerPort { get; set; } = 53;
+
+    /// <summary>
+    /// Gets or sets the default port assigned to A-record endpoints when no SRV record
+    /// supplies an explicit port number.
+    /// </summary>
+    public int DefaultPort { get; set; } = 443;
+
+    /// <summary>Gets or sets the URI scheme assumed for A-record-derived endpoints.</summary>
+    public string DefaultScheme { get; set; } = "https";
+
+    /// <summary>Gets or sets the maximum number of UDP retry attempts on transient DNS failures.</summary>
+    public int MaxRetries { get; set; } = 2;
+
+    /// <summary>Gets or sets the UDP socket receive timeout applied per attempt.</summary>
+    public TimeSpan SocketTimeout { get; set; } = TimeSpan.FromSeconds(2);
+}
+
+/// <summary>HTTP service registry (Consul-compatible API) discovery settings.</summary>
+public sealed class RegistryDiscoveryOptions
+{
+    /// <summary>Gets or sets the base URL of the registry agent (e.g. <c>http://localhost:8500</c>).</summary>
+    public string AgentEndpoint { get; set; } = "http://localhost:8500";
+
+    /// <summary>Gets or sets the ACL token sent in the <c>X-Consul-Token</c> header.</summary>
+    public string? AclToken { get; set; }
+
+    /// <summary>Gets or sets whether only instances with passing health checks are returned.</summary>
+    public bool OnlyHealthyInstances { get; set; } = true;
+
+    /// <summary>Gets or sets the target datacenter. Uses the agent default when <see langword="null"/>.</summary>
+    public string? Datacenter { get; set; }
+
+    /// <summary>Gets or sets how often TTL heartbeats are sent for self-registered service entries.</summary>
+    public TimeSpan HeartbeatInterval { get; set; } = TimeSpan.FromSeconds(10);
+}
+
+/// <summary>Controls automatic self-registration of the current service instance on startup.</summary>
+public sealed class SelfRegistrationOptions
+{
+    /// <summary>Gets or sets whether self-registration runs when the application starts.</summary>
+    public bool Enabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets the logical service name to register under.
+    /// Defaults to the entry assembly name when <see langword="null"/>.
+    /// </summary>
+    public string? ServiceName { get; set; }
+
+    /// <summary>Gets or sets the version tag advertised in the registry entry.</summary>
+    public string? Version { get; set; }
+
+    /// <summary>
+    /// Gets or sets the host name or IP address to advertise.
+    /// Uses <see cref="System.Net.Dns.GetHostName()"/> when <see langword="null"/>.
+    /// </summary>
+    public string? AdvertiseHost { get; set; }
+
+    /// <summary>Gets or sets the port to advertise during registration.</summary>
+    public int AdvertisePort { get; set; } = 443;
+
+    /// <summary>Gets or sets the URI scheme advertised in the registration entry.</summary>
+    public string AdvertiseScheme { get; set; } = "https";
+
+    /// <summary>Gets or sets the HTTP health check path used for registry-driven HTTP probes.</summary>
+    public string HealthCheckPath { get; set; } = "/health";
+
+    /// <summary>Gets or sets arbitrary tags to attach to the registration entry.</summary>
+    public List<string> Tags { get; set; } = [];
+}
+
+/// <summary>Specifies which resolution strategy the discovery engine employs.</summary>
+public enum DiscoveryMode
+{
+    /// <summary>Resolve service instances exclusively via DNS SRV/A records.</summary>
+    Dns = 0,
+    /// <summary>Resolve service instances exclusively via the HTTP service registry.</summary>
+    Registry = 1,
+    /// <summary>Attempt the registry first; fall back to DNS on failure or empty results.</summary>
+    Hybrid = 2
+}
+
+/// <summary>Load-balancing algorithm used when selecting a single endpoint from multiple instances.</summary>
+public enum LoadBalancingStrategy
+{
+    /// <summary>Distributes requests sequentially across all healthy instances.</summary>
+    RoundRobin = 0,
+    /// <summary>Selects a uniformly random healthy instance per request.</summary>
+    Random = 1,
+    /// <summary>Routes traffic proportionally to each instance's <c>Weight</c> value.</summary>
+    Weighted = 2,
+    /// <summary>Always selects the instance with the lowest <c>Priority</c> value; ties broken by index order.</summary>
+    Priority = 3
+}
