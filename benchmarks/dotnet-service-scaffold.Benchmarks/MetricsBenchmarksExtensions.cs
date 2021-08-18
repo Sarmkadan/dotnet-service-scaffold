a8f470de5311fa9c9be581336c87902ebaba1e86
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-18 08:18:32 +0300

    Add extension helpers for MetricsBenchmarks

--- benchmarks/dotnet-service-scaffold.Benchmarks/MetricsBenchmarksExtensions.cs
+++ benchmarks/dotnet-service-scaffold.Benchmarks/MetricsBenchmarksExtensions.cs
@@ -4,13 +4,25 @@ using System.Threading.Tasks;
 
 namespace DotnetServiceScaffold.Benchmarks
 {
+    /// <summary>
+    /// Provides extension methods for the MetricsBenchmarks class.
+    /// </summary>
     public static class MetricsBenchmarksExtensions
     {
+        /// <summary>
+        /// Resets the metrics for the MetricsBenchmarks instance.
+        /// </summary>
+        /// <param name="benchmarks">The MetricsBenchmarks instance to reset.</param>
         public static void ResetMetrics(this MetricsBenchmarks benchmarks)
         {
             benchmarks.Setup();
         }
 
+        /// <summary>
+        /// Verifies that the metrics recorded by the MetricsBenchmarks instance are not empty.
+        /// </summary>
+        /// <param name="benchmarks">The MetricsBenchmarks instance to verify.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public static async Task VerifyMetricsNotEmptyAsync(this MetricsBenchmarks benchmarks)
         {
             var metrics = await benchmarks.GetMetrics();
@@ -20,6 +32,12 @@ namespace DotnetServiceScaffold.Benchmarks
             }
         }
 
+        /// <summary>
+        /// Asserts that a specific metric exists in the metrics recorded by the MetricsBenchmarks instance.
+        /// </summary>
+        /// <param name="benchmarks">The MetricsBenchmarks instance to verify.</param>
+        /// <param name="metricName">The name of the metric to assert exists.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public static async Task AssertMetricExistsAsync(this MetricsBenchmarks benchmarks, string metricName)
         {
             var metrics = await benchmarks.GetMetrics();
