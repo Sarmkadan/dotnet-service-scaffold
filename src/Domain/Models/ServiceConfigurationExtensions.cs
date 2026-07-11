84429bc985318e074381ff6640db63af684e349a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-11 16:50:03 +0000

    refactor(models): tighten guard clauses and docs in ServiceConfigurationExtensions

--- src/Domain/Models/ServiceConfigurationExtensions.cs
+++ src/Domain/Models/ServiceConfigurationExtensions.cs
@@ -12,7 +12,7 @@ using DotnetServiceScaffold.Domain.Models;
 namespace DotnetServiceScaffold.Domain.Models;
 
 /// <summary>
-/// Extension methods for ServiceConfiguration to provide additional functionality
+/// Extension methods for <see cref="ServiceConfiguration"/> that provide additional functionality
 /// for working with configuration values in common scenarios.
 /// </summary>
 public static class ServiceConfigurationExtensions
@@ -21,9 +21,12 @@ public static class ServiceConfigurationExtensions
     /// Gets the configuration value as a double. Returns 0 if parsing fails.
     /// </summary>
     /// <param name="configuration">The service configuration</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>The parsed double value or 0 if parsing fails</returns>
     public static double GetDoubleValue(this ServiceConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         if (double.TryParse(configuration.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result))
         {
             return result;
@@ -36,9 +39,12 @@ public static class ServiceConfigurationExtensions
     /// Gets the configuration value as a decimal. Returns 0 if parsing fails.
     /// </summary>
     /// <param name="configuration">The service configuration</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>The parsed decimal value or 0 if parsing fails</returns>
     public static decimal GetDecimalValue(this ServiceConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         if (decimal.TryParse(configuration.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result))
         {
             return result;
@@ -51,9 +57,12 @@ public static class ServiceConfigurationExtensions
     /// Gets the configuration value as a DateTime. Returns DateTime.MinValue if parsing fails.
     /// </summary>
     /// <param name="configuration">The service configuration</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>The parsed DateTime value or DateTime.MinValue if parsing fails</returns>
     public static DateTime GetDateTimeValue(this ServiceConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         if (DateTime.TryParse(configuration.Value, out var result))
         {
             return result;
@@ -66,9 +75,12 @@ public static class ServiceConfigurationExtensions
     /// Gets the configuration value as a Guid. Returns Guid.Empty if parsing fails.
     /// </summary>
     /// <param name="configuration">The service configuration</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>The parsed Guid value or Guid.Empty if parsing fails</returns>
     public static Guid GetGuidValue(this ServiceConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         if (Guid.TryParse(configuration.Value, out var result))
         {
             return result;
@@ -84,9 +96,12 @@ public static class ServiceConfigurationExtensions
     /// <param name="configuration">The service configuration</param>
     /// <param name="newValue">The new value to set</param>
     /// <param name="userId">Optional user ID tracking who made the change</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>True if the value was updated, false if the value was the same</returns>
     public static bool UpdateValueIfChanged(this ServiceConfiguration configuration, string newValue, Guid? userId = null)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         if (string.Equals(configuration.Value, newValue, StringComparison.Ordinal))
         {
             return false;
@@ -102,19 +117,23 @@ public static class ServiceConfigurationExtensions
     /// </summary>
     /// <param name="configuration">The service configuration</param>
     /// <param name="defaultValue">The default value to return if configuration is null or empty</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>The configuration value or the default value</returns>
     public static string GetValueOrDefault(this ServiceConfiguration? configuration, string defaultValue = "")
     {
-        return configuration?.Value ?? defaultValue;
+        ArgumentNullException.ThrowIfNull(configuration);
+        return configuration.Value ?? defaultValue;
     }
 
     /// <summary>
     /// Determines if the configuration is a system configuration that should not be modified by users.
     /// </summary>
     /// <param name="configuration">The service configuration</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>True if this is a system configuration; otherwise false</returns>
     public static bool IsSystemConfiguration(this ServiceConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(configuration);
         return configuration.IsSystemConfig || configuration.Key.StartsWith("System.", StringComparison.Ordinal);
     }
 
@@ -124,9 +143,12 @@ public static class ServiceConfigurationExtensions
     /// <typeparam name="T">The enum type to parse</typeparam>
     /// <param name="configuration">The service configuration</param>
     /// <param name="defaultValue">The default value to return if parsing fails</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is null.</exception>
     /// <returns>The parsed enum value or the default value if parsing fails</returns>
     public static T GetEnumValue<T>(this ServiceConfiguration configuration, T defaultValue = default) where T : struct, Enum
     {
+        ArgumentNullException.ThrowIfNull(configuration);
+
         if (Enum.TryParse<T>(configuration.Value, true, out var result))
         {
             return result;
