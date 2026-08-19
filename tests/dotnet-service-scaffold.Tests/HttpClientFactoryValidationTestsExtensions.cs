0339d2ab2e151a1653873846f1d98cfe7a52fe69
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-19 14:12:21 +0000

    chore: apply workspace updates

---
+++ tests/dotnet-service-scaffold.Tests/HttpClientFactoryValidationTestsExtensions.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+using DotnetServiceScaffold.Infrastructure.Integration;
+
+namespace DotnetServiceScaffold.Tests
+{
+    public static class HttpClientFactoryValidationTestsExtensions
+    {
+        // Add your extension methods here
+    }
+}
