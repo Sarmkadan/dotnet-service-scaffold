178f7696a814d56dbadd68ce82de73bc3f26b9d6
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-23 19:53:32 +0000

    feat: add empty EncryptionUtilityJsonExtensionsValidation

---
+++ src/Shared/Utilities/EncryptionUtilityJsonExtensionsValidation.cs
@@ -0,0 +1,10 @@
+#nullable enable
+
+namespace DotnetServiceScaffold.Shared.Utilities;
+
+/// <summary>
+/// Provides validation helpers for <see cref="EncryptionUtilityJsonExtensions"/>.
+/// </summary>
+public static class EncryptionUtilityJsonExtensionsValidation
+{
+}
