3069c736c1ede99ca863b08de7683fa0dce1dd33
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-26 04:06:34 +0000

    chore: add empty constants class for ServiceMeshOptionsExtensions

---
+++ src/Infrastructure/ServiceMesh/ServiceMeshOptionsExtensionsConstants.cs
@@ -0,0 +1,15 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+namespace DotnetServiceScaffold.Infrastructure.ServiceMesh;
+
+/// <summary>
+/// Constants used by <see cref="ServiceMeshOptionsExtensions"/>.
+/// </summary>
+internal static class ServiceMeshOptionsExtensionsConstants
+{
+}
