0fd88bd9f96c52372cc6971e52be20f7cacc29dd
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-26 01:47:43 +0000

    chore: add empty constants class for StructuredLoggingOptionsExtensions

---
+++ src/Infrastructure/Logging/StructuredLoggingOptionsExtensionsConstants.cs
@@ -0,0 +1,14 @@
+#nullable enable
+
+namespace DotnetServiceScaffold.Infrastructure.Logging;
+
+/// <summary>
+/// Constants used by <see cref="StructuredLoggingOptionsExtensions"/>.
+/// </summary>
+internal static class StructuredLoggingOptionsExtensionsConstants
+{
+    // No magic values were found in StructuredLoggingOptionsExtensions.cs,
+    // so this class remains empty. If new literals or numbers are added
+    // to StructuredLoggingOptionsExtensions in the future, they should be
+    // defined here and referenced from that file.
+}
