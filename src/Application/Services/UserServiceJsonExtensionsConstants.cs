c5cf2f6c487717a42171aa0f0204cad381ab9116
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-27 11:35:02 +0000

    refactor: extract magic values from UserServiceJsonExtensions

---
+++ src/Application/Services/UserServiceJsonExtensionsConstants.cs
@@ -0,0 +1,11 @@
+#nullable enable
+
+namespace DotnetServiceScaffold.Application.Services;
+
+/// <summary>
+/// Constants for <see cref="UserServiceJsonExtensions"/>.
+/// </summary>
+internal static class UserServiceJsonExtensionsConstants
+{
+    // No magic values or repeated string literals were found in UserServiceJsonExtensions.cs.
+}
