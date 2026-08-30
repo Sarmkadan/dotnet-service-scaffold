7248e70ca4d22c0934037558a0b57f304278e797
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 20:07:56 +0000

    feat: extract magic values to constants in CacheAndCollectionTestsJsonExtensionsTests

---
+++ tests/dotnet-service-scaffold.Tests/CacheAndCollectionTestsJsonExtensionsConstants.cs
@@ -0,0 +1,8 @@
+namespace DotnetServiceScaffold.Tests;
+
+/// <summary>
+/// Constants for <see cref="CacheAndCollectionTestsJsonExtensions"/>.
+/// </summary>
+internal static class CacheAndCollectionTestsJsonExtensionsConstants
+{
+}
