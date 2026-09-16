a22d082000eeef42f6e3327ec6d6c94c4faef4fd
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-16 09:56:34 +0000

    chore: maintenance updates

--- src/Domain/Models/HealthCheckResult.cs
+++ src/Domain/Models/HealthCheckResult.cs
@@ -171,6 +171,6 @@ public sealed class HealthCheckResult : IHealthCheckResult, IEquatable<HealthChe
     /// <returns>A string that represents the current object.</returns>
     public override string ToString()
     {
-        return $"HealthCheckResult {{ Id = {Id}, ServiceId = {ServiceId}, Service = {Service}, Status = {Status}, HttpStatusCode = {HttpStatusCode}, ResponseTimeMs = {ResponseTimeMs} }}";
+        return $"HealthCheckResult {{ Id = {Id}, ServiceId = {ServiceId}, Status = {Status}, HttpStatusCode = {HttpStatusCode}, ResponseTimeMs = {ResponseTimeMs}, CheckedAt = {CheckedAt} }}";
     }
 }
